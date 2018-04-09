63402eb9c1a0364fdafeef2ebec9da4535a2426d
Charles Puelz <charles.puelz@gmail.com>
2018-04-09 16:33:30 -0400

    added code for velocity

--- translating_web/main.cpp
+++ translating_web/main.cpp
@@ -270,24 +270,30 @@ int main(int argc, char** argv)
         // Open streams to save volume of structure.
         ofstream volume_stream;
         ofstream pressure_stream;
+        ofstream velocity_stream;
         if (SAMRAI_MPI::getRank() == 0)
         {
             volume_stream.open("volume.curve", ios_base::out | ios_base::trunc);
-            pressure_stream.open("pressure.curve", ios_base::out | ios_base::trunc);
+            pressure_stream.open("pressure.dat", ios_base::out | ios_base::trunc);
+            velocity_stream.open("velocity.dat", ios_base::out | ios_base::trunc);
         }
         
         // setting up some objects for computing mean pressure
         VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
         Pointer<hier::Variable<NDIM> > p_var = navier_stokes_integrator->getPressureVariable();
         Pointer<VariableContext> p_current_ctx = navier_stokes_integrator->getCurrentContext();
+        Pointer<hier::Variable<NDIM> > u_var = navier_stokes_integrator->getVelocityVariable();
+        Pointer<VariableContext> u_current_ctx = navier_stokes_integrator->getCurrentContext();
         const int p_current_idx = var_db->mapVariableAndContextToIndex(p_var, p_current_ctx);
-        FEDataManager::InterpSpec p_interp_spec("PIECEWISE_LINEAR",
+        const int u_current_idx = var_db->mapVariableAndContextToIndex(u_var, u_current_ctx);
+        FEDataManager::InterpSpec interp_spec("PIECEWISE_LINEAR",
                                                 QGAUSS,
                                                 FIFTH,
                                                 /*use_adaptive_quadrature*/ false,
                                                 /*point_density*/ 2.0,
                                                 /*use_consistent_mass_matrix*/ true);
-        libMesh::DenseVector<double> MeanData;
+        libMesh::DenseVector<double> MeanPressureData;
+        libMesh::DenseVector<double> MeanVelocityData;
         const double current_time = 0.0;
         Pointer<SideVariable<NDIM, double> > u_copy_var = new SideVariable<NDIM, double>("u_copy");
         Pointer<CellVariable<NDIM, double> > p_copy_var = new CellVariable<NDIM, double>("p_copy");
@@ -331,31 +337,51 @@ int main(int argc, char** argv)
             pout << "\n";
             
             // **********************************************
-            // get mean pressure on surface mesh
+            // get mean pressure and velocity on surface mesh
             //***********************************************
             HierarchyDataOpsManager<NDIM>* hier_data_ops_manager = HierarchyDataOpsManager<NDIM>::getManager();
             Pointer<HierarchyDataOpsReal<NDIM, double> > hier_cc_data_ops = hier_data_ops_manager->getOperationsDouble(p_var, patch_hierarchy, true);
+            Pointer<HierarchyDataOpsReal<NDIM, double> > hier_sc_data_ops = hier_data_ops_manager->getOperationsDouble(u_var, patch_hierarchy, true);
             hier_cc_data_ops->copyData(p_copy_idx, p_current_idx, true);
+            hier_sc_data_ops->copyData(u_copy_idx, u_current_idx, true);
               
-            // fill ghost cells
+            // fill ghost cells for pressure
             typedef HierarchyGhostCellInterpolation::InterpolationTransactionComponent InterpolationTransactionComponent;
-            std::vector<InterpolationTransactionComponent> transaction_comp(1);
-            transaction_comp[0] = InterpolationTransactionComponent(p_copy_idx,
+            std::vector<InterpolationTransactionComponent> p_transaction_comp(1);
+            p_transaction_comp[0] = InterpolationTransactionComponent(p_copy_idx,
                                                                     "LINEAR_REFINE",
                                                                     /*use_cf_bdry_interpolation*/ false,
                                                                     "CONSERVATIVE_COARSEN",
                                                                     "LINEAR");
                                                                                            
-            Pointer<HierarchyGhostCellInterpolation> hier_bdry_fill = new HierarchyGhostCellInterpolation();
-            hier_bdry_fill->initializeOperatorState(transaction_comp, patch_hierarchy);
-            hier_bdry_fill->fillData(loop_time);
+            Pointer<HierarchyGhostCellInterpolation> p_hier_bdry_fill = new HierarchyGhostCellInterpolation();
+            p_hier_bdry_fill->initializeOperatorState(p_transaction_comp, patch_hierarchy);
+            p_hier_bdry_fill->fillData(loop_time);
+            
+            // fill ghost cells for velocity
+            std::vector<InterpolationTransactionComponent> u_transaction_comp(1);
+            u_transaction_comp[0] = InterpolationTransactionComponent(u_copy_idx,
+                                                                    "CONSERVATIVE_LINEAR_REFINE",
+                                                                    /*use_cf_bdry_interpolation*/ false,
+                                                                    "CONSERVATIVE_COARSEN",
+                                                                    "LINEAR");
+                                                                                           
+            Pointer<HierarchyGhostCellInterpolation> u_hier_bdry_fill = new HierarchyGhostCellInterpolation();
+            u_hier_bdry_fill->initializeOperatorState(u_transaction_comp, patch_hierarchy);
+            u_hier_bdry_fill->fillData(loop_time);
                         
             System& X_system = equation_systems->get_system<System>(IBFEMethod::COORDS_SYSTEM_NAME);
             NumericVector<double>* X_vec = X_system.solution.get();
             NumericVector<double>* X_ghost_vec = X_system.current_local_solution.get();
             X_vec->localize(*X_ghost_vec);
-            fe_data_manager->readData(p_copy_idx, MeanData, *X_ghost_vec, p_interp_spec);
-            pressure_stream << loop_time << " " << MeanData(0) << "\n";
+            fe_data_manager->readData(p_copy_idx, MeanPressureData, *X_ghost_vec, interp_spec);
+            fe_data_manager->readData(u_copy_idx, MeanVelocityData, *X_ghost_vec, interp_spec);
+            pressure_stream << loop_time << " " << MeanPressureData(0) << "\n";
+            velocity_stream << loop_time;
+            for(int dd = 0; dd < NDIM; ++dd) velocity_stream << " " << MeanVelocityData(dd);
+            velocity_stream << "\n";
+            
+            // see if we need to reallocate
             HierarchyMathOps hier_math_ops("HierarchyMathOps", patch_hierarchy);
             hier_math_ops.setPatchHierarchy(patch_hierarchy);
             hier_math_ops.resetLevels(coarsest_ln, finest_ln);
@@ -417,6 +443,7 @@ int main(int argc, char** argv)
         {
             volume_stream.close();
             pressure_stream.close();
+            velocity_stream.close();
         }
 
         // Cleanup Eulerian boundary condition specification objects (when
